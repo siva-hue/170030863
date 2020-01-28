e8443a46a6c6d8f63ce34b4e17a57c58351eb86b
siva-hue <60382486+siva-hue@users.noreply.github.com>
2020-01-28 14:48:16 +0530

    version1

---
+++ Github2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Github2
+{
+public static void main(String[] args)
+{
+int n=Integer.parseInt(args[0]);
+int n1=Integer.parseInt(args[1]);
+int s=n+n1;
+System.out.println(s);
+}
+}
